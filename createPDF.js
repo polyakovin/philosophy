61a0cf28e6358d3967fa2e48d1d8856097689482
iGor Polyakov <igor_polyakov@phystech.edu>
2017-06-13 12:11:23 +0300

    script updated

--- createPDF.js
+++ createPDF.js
@@ -1,25 +1,43 @@
 const markdownpdf = require("markdown-pdf");
 const dateFormat = require('dateformat');
-const toc = require('markdown-toc');
 
 const time = dateFormat(new Date(), "yyyy.mm.dd");
-const bookPath = `./Seminars_${time}.pdf`;
-
-let mdDocs = [];
-for (let i = 1; i <= 26; i++) {
-  mdDocs.push(`Seminars/Seminar_${i < 10 ? "0" + i : i}.md`)
-}
 
 const options = {
   remarkable: {
     breaks: false,
     preset: 'full'
-    // plugins: [toc]
   },
 };
 
+
+
+const seminarsPDF = `./Seminars_${time}.pdf`;
+
+let seminarsMD = [];
+for (let i = 1; i <= 26; i++) {
+  seminarsMD.push(`Seminars/Seminar_${i < 10 ? "0" + i : i}.md`)
+}
+
 markdownpdf(options).concat
-  .from(mdDocs)
-  .to(bookPath, function () {
-  console.log("Created", bookPath)
-})
+  .from(seminarsMD)
+  .to(seminarsPDF, () => {
+    console.log("Created", seminarsPDF);
+});
+
+
+
+const lecturesPDF = `./Lectures_${time}.pdf`;
+
+let lecturesMD = [];
+for (let i = 1; i <= 22; i++) {
+  if (i !== 14 && (i === 1 || i > 5)) {
+    lecturesMD.push(`Lectures/Lecture_${i < 10 ? "0" + i : i}.md`);
+  }
+}
+
+markdownpdf(options).concat
+  .from(lecturesMD)
+  .to(lecturesPDF, () => {
+    console.log("Created", lecturesPDF);
+});
